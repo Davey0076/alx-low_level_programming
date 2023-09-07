4ec25bad6ab52c1e4c3afade30cd25485367912c
David Okoth <okoth.david21@students.dkut.ac.ke>
2023-09-07 15:07:45 -0700

    Updates on Hello puts task betty installation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-/*
+/**
  *main-entry point
  *
  * Return: Always 0 (Sucess)
- * */
+ */
 int main(void)
 {
-	puts("Programming is like a multillingual puzzle");
+	puts("Programming is like a multilingual puzzle\n");
 	return (0);
 }
