d1f110232d411dc1239ad2b6b04e1efcea10a18a
David Okoth <okoth.david21@students.dkut.ac.ke>
2023-09-15 23:56:27 -0700

    UNIX fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,12 +8,6 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
-	ssize_t bytes_written = write(STDERR_FILENO, message, strlen(message));
-
-	if (bytes_written == -1) {
-		perror("write");
-		return 1;
-	}
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
