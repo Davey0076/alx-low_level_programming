ace814157fc0d0bbde59e783db0937b2db88b009
David Okoth <okoth.david21@students.dkut.ac.ke>
2023-09-16 00:17:54 -0700

    print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,15 @@ int main(void)
 {
 	int n;
 
-	n = 48;
-	while (n <= 57)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-		if (n == 57)
-			continue;
-		putchar(',');
-		putchar(' ');
-		n++;
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
