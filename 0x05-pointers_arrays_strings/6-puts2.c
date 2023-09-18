d98330ebf26168009bcfbed2fed9861b074cfc87
David Okoth <okoth.david21@students.dkut.ac.ke>
2023-09-18 05:15:06 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print every other character
+ * @str: string to be printed
+ * Return : void
+ */
+void puts2(char *str)
+{
+	int count = 0;
+	int z = 0;
+	char *p = str;
+	int n;
+
+	while (*p != '\0')
+	{
+		p++;
+		count = count + 1;
+	}
+	z = count - 1;
+	for (n = 0; n <= z; n++)
+	{
+		if (n % 2 == 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n');
+}
