5a0ac3f176ddc63a88b1315d7765ea4fe1b044f4
David Okoth <okoth.david21@students.dkut.ac.ke>
2023-09-18 04:40:00 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse order
+ * @s: string to be printed in reverse
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		count = count + 1;
+		s++;
+	}
+	s--;
+	for (i = count; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
