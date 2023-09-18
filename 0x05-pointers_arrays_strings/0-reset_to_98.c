f6c5c090a44253164ee01c710cb3123915bc9ab2
David Okoth <okoth.david21@students.dkut.ac.ke>
2023-09-18 02:40:19 -0700

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - funcion that takes pointer and updates value
+ * @n: integer value that takes pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README file on pointers, arrays and strings in C 
